79392af1c1911372b5a1fe09715b5e47b5b3c50d
Cyrill <cybucher@students.ethz.ch>
2022-05-29 17:10:54 +0200

    Video Tutorial ep1: sprite
    - need to create a texture atlas
    - aspect ratio of the sprite is wrong

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
+
+pub const BG_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+pub const ASPECT_RATIO: f32 = 16.0 / 9.0;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HexRailsPlugin)
+        .run();
+}
+pub struct HexRailsPlugin;
+
+impl Plugin for HexRailsPlugin {
+    fn build(&self, app: &mut App) {
+        let height = 900.0;
+
+        app.insert_resource(ClearColor(BG_COLOR))
+            .insert_resource(WindowDescriptor {
+                width: height * ASPECT_RATIO,
+                height: height,
+                title: "Hex Rails".to_string(),
+                present_mode: bevy::window::PresentMode::Fifo,
+                resizable: false,
+                ..Default::default()
+            })
+            .add_startup_system_to_stage(StartupStage::PreStartup, load_sprite)
+            .add_startup_system(print_version)
+            .add_startup_system(spawn_camera)
+            .add_startup_system(spawn_tiles);
+    }
+}
 
 #[derive(Component)]
 struct TileCoordinate((i32, i32));
@@ -32,7 +63,21 @@ impl TileCoordinate {
 #[derive(Component)]
 struct Tile;
 
-fn add_tiles(mut commands: Commands) {
+fn spawn_tiles(mut commands: Commands, atlas_h: Res<AtlasHandle>) {
+    let mut sprite = TextureAtlasSprite::new(0);
+    sprite.custom_size = Some(Vec2::splat(0.5));
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: sprite,
+            texture_atlas: atlas_h.0.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 100.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Name::new("idk"));
+
     commands.spawn().insert(Tile).insert(TileCoordinate((0, 0)));
     commands
         .spawn()
@@ -44,38 +89,29 @@ fn add_tiles(mut commands: Commands) {
         .insert(TileCoordinate((0, 0)).west());
 }
 
-struct WeirdTimer(Timer);
-
-fn all_coords(
-    time: Res<Time>,
-    mut timer: ResMut<WeirdTimer>,
-    query: Query<&TileCoordinate, With<Tile>>,
-) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {:?}!", name.0);
-        }
-    }
-}
-
 fn print_version() {
     println!("Build with Bevy!");
 }
 
-pub struct HexRailsPlugin;
-
-impl Plugin for HexRailsPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(WeirdTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(print_version)
-            .add_startup_system(add_tiles)
-            .add_system(all_coords);
-    }
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+    camera.orthographic_projection.left = ASPECT_RATIO * -1.0;
+    camera.orthographic_projection.right = ASPECT_RATIO * 1.0;
+    camera.orthographic_projection.scaling_mode = ScalingMode::FixedVertical;
+    commands.spawn_bundle(camera);
 }
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HexRailsPlugin)
-        .run();
+struct AtlasHandle(Handle<TextureAtlas>);
+
+fn load_sprite(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas: ResMut<Assets<TextureAtlas>>,
+) {
+    let image = asset_server.load("Grasstile.png");
+    let atlas = TextureAtlas::from_grid(image, Vec2::new(444.0, 512.0), 1, 1);
+    let atlas_handle = texture_atlas.add(atlas);
+    commands.insert_resource(AtlasHandle(atlas_handle));
 }
